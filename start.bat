db45d573a48035cae6d9a2ba237e30f0d3ce20c6
godlyrblx <itsgodlyrbx@gmail.com>
2025-10-19 12:01:18 +0200

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,3 +1,14 @@
 @echo off
+set /p COOKIE=Enter Cookie value: 
+
+(
+    echo {
+    echo     "Main_Cookie": {
+    echo         "Cookie": "%COOKIE%",
+    echo         "Bypass": false
+    echo     }
+    echo }
+) > settings.json
+
 python main.py
 pause
